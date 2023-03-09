f89dd83e6dfefedccba934d9e6bff66d4d4b664f
Victor Cruz <103138539+VictrCruz312@users.noreply.github.com>
2023-03-09 11:45:02 -0300

    fix: advert

--- src/services/adverts/advertList.service.ts
+++ src/services/adverts/advertList.service.ts
@@ -4,7 +4,7 @@ import { Advert } from "../../entities/advert.entity";
 const advertListService = async () => {
     const advertRepository = AppDataSource.getRepository(Advert);
     const adverts = await advertRepository.find({
-        relations: { images: true },
+        relations: { images: true, user: true },
     });
     return adverts;
 };
